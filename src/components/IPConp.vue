764dd4de32bff4abd1d7a8c5e076104435301f15
Kol Crooks <kolcrooks10@gmail.com>
2019-05-30 22:16:05 -0400

    init commit

---
+++ src/components/IPConp.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>{{ip}}   {{msg}}</div>
+  </div>
+</template>
+
+<script>
+let tools = require('../js/tools.js')
+
+export default {
+  props: ["msg"],
+  name: 'IPConp',
+  data () {
+    return {
+      title: "A test site",
+      ip: "test"
+    }
+  },
+  methods: {
+    getIp: async ()=>{
+      let ip = await tools.findIp();
+      return ip;
+    }
+  },
+  created(){
+    var _this = this;
+    this.getIp().then((ip)=>{
+      _this.ip = ip;
+    })
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <h1>{{ title }}</h1>
+      <h2>Here is your ip:</h2>
+      <client-ip :msg="msgg"></client-ip>
+    </div>
+</template>
+
+<script>
+import IPConp from './IPConp.vue';
+
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      title: "A test site",
+      msgg: "test2321"
+    }
+  },
+  components:{
+      "client-ip": IPConp
+  }
+}
+</script>
